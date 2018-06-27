bbae28e53e4bb822068713ae7cde493245571730
Aleksei Petukhov <aleksei.petukhov@birkle-it.com>
2018-06-27 15:09:11 +0200

    Addes some comments

--- ai.py
+++ ai.py
@@ -15,14 +15,16 @@ from torch.autograd import Variable
 # Creating the architecture of the Neural Network
 
 class Network(nn.Module):
-    
+
     def __init__(self, input_size, nb_action):
         super(Network, self).__init__()
         self.input_size = input_size
         self.nb_action = nb_action
-        self.fc1 = nn.Linear(input_size, 30)
+        self.fc1 = nn.Linear(input_size, 30) # connect all neurons in input layer to the hidden layer
+        # input size is 5 (sig1, sig2, sig3, orient, -orient)
+        # after experimenting and parameter tuning, 30 seems to be decent value
         self.fc2 = nn.Linear(30, nb_action)
-    
+
     def forward(self, state):
         x = F.relu(self.fc1(state))
         q_values = self.fc2(x)
@@ -31,24 +33,24 @@ class Network(nn.Module):
 # Implementing Experience Replay
 
 class ReplayMemory(object):
-    
+
     def __init__(self, capacity):
         self.capacity = capacity
         self.memory = []
-    
+
     def push(self, event):
         self.memory.append(event)
         if len(self.memory) > self.capacity:
             del self.memory[0]
-    
+
     def sample(self, batch_size):
-        samples = zip(*random.sample(self.memory, batch_size))
+        samples = zip(*random.sample(self.memory, batch_size)) # * unzips
         return map(lambda x: Variable(torch.cat(x, 0)), samples)
 
 # Implementing Deep Q Learning
 
 class Dqn():
-    
+
     def __init__(self, input_size, nb_action, gamma):
         self.gamma = gamma
         self.reward_window = []
@@ -58,12 +60,12 @@ class Dqn():
         self.last_state = torch.Tensor(input_size).unsqueeze(0)
         self.last_action = 0
         self.last_reward = 0
-    
+
     def select_action(self, state):
         probs = F.softmax(self.model(Variable(state, volatile = True))*100) # T=100
         action = probs.multinomial()
         return action.data[0,0]
-    
+
     def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
         outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
         next_outputs = self.model(batch_next_state).detach().max(1)[0]
@@ -72,7 +74,7 @@ class Dqn():
         self.optimizer.zero_grad()
         td_loss.backward(retain_variables = True)
         self.optimizer.step()
-    
+
     def update(self, reward, new_signal):
         new_state = torch.Tensor(new_signal).float().unsqueeze(0)
         self.memory.push((self.last_state, new_state, torch.LongTensor([int(self.last_action)]), torch.Tensor([self.last_reward])))
@@ -87,15 +89,15 @@ class Dqn():
         if len(self.reward_window) > 1000:
             del self.reward_window[0]
         return action
-    
+
     def score(self):
         return sum(self.reward_window)/(len(self.reward_window)+1.)
-    
+
     def save(self):
         torch.save({'state_dict': self.model.state_dict(),
                     'optimizer' : self.optimizer.state_dict(),
                    }, 'last_brain.pth')
-    
+
     def load(self):
         if os.path.isfile('last_brain.pth'):
             print("=> loading checkpoint... ")
